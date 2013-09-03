a262177277b20161871850823fe7f7a754a759c9
kinfongiscool <bradley.kin.fong@gmail.com>
2013-09-03 15:11:00 -0500

    added .java for fragments. began changing MainActivity for fragments

---
+++ AustinRainHour/src/main/java/com/realityexpander/austinrainhour/WeatherInfoFragment.java
@@ -0,0 +1,22 @@
+package com.realityexpander.austinrainhour;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kin on 9/3/13.
+ */
+public class WeatherInfoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_time_prob_intense, container, false);
+
+        // put stuff here
+
+        return rootView;
+    }
+}
